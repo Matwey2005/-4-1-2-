9bb86045acb61c9bf85540a36ef1f71d7ad1aa2b
shag <92595243+Matwey2005@users.noreply.github.com>
2024-03-21 12:50:41 +0300

    Add files via upload

---
+++ Практ4 1 задание.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream> 
+#include <iostream> 
+using namespace std;
+
+/*int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n[5];
+	int x = 0;
+	//Ввод чисел
+	for (int i = 0; i < 5; i++) {
+
+		cout << "Введите число";
+		cin >> n[i];
+	}
+	//деление на 5
+	for (int i = 0; i < 5; i++) {
+		if (n[i] % 5 == 0) {
+			x += n[i];
+		}
+	}
+	//Вычисление суммы
+	if (x!= 0) {
+		cout << "Сумма чисел="<<x;
+	}
+	else {
+		cout << "error" << endl;
+	}
+	return 0;
+} */
+
+/*int main() {
+	setlocale(LC_ALL, "Russian");
+	int n[5];
+	int x = 0;
+	int i = 0;
+	//Вводиим числа
+	while (i < 5) {
+		cout << "Введите число";
+		cin >> n[i];
+		i++;
+
+	}
+	i = 0;
+	//деление на 5
+	while (i < 5) {
+		if (n[i] % 5 == 0) {
+			x += n[i];
+		}
+		i++;
+	
+	}
+	//проверка чисел /5 
+	if (x != -0) {
+		cout << "Сумма чисел"<<x<< endl;
+	} 
+	else {
+		cout << "error" << endl; 
+
+	}
+	return 0; 
+} */ 
+int main() {
+	int n[5];
+	int x; 
+	int i = 0;
+	//Ввод чисел 
+	do {
+		cout << "Введите число";
+		cin>>n[i];
+		i++;
+	} while (i < 5);
+	i = 0;
+//вычесление суммы 
+	do {
+		if (n[i] % 5 == 0) {
+			x += n[i];
+
+		}
+		i++;
+	} while (i < 5); 
+	//проверка 
+	if(x != 0) {
+		cout << "Сумма чисел:" << x << endl; 
+	}
+	else {
+		cout << "errror" << endl; 
+	}
+	return 0;
+
+}
---
+++ Практическая 4 задание2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std; 
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+	int n[5]; 
+	int pol;
+	int otr; 
+	for (int i = 0; i < 5; i++) {
+		cout << "Введите число ";
+			cin>> n[5];
+		if (n[i] > 0) {
+			pol++;
+		}
+		else if (n[5] < 0) {
+			otr++;
+		}
+	}
+	if (pol == 0 && otr == 0) {
+		cout << "error" << endl; 
+	}
+	else {
+		cout << "Количество положительных чисел" << pol << endl;
+		cout << "Количество отрицательных чисел" << otr << endl; 
+	}
+	return 0; 
+}
